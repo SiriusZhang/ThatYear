7bd07530eeed402812f2daf2307b2e50cc9a546c
SiriusZhang <lang100tuteng@qq.com>
2017-04-16 12:01:23 +0800

    first commit

---
+++ ThatYear/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThatYear
+//
+//  Created by jiaxu zhang on 2017/4/16.
+//  Copyright © 2017年 jiaxu zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
